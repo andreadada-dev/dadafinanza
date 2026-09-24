3dcc3d9533572c21a487d9bedf9304166ac01488
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-25 00:34:01 +0200

    test: add personal settings responsive scaffold

---
+++ test/personal_settings_responsive_test.dart
@@ -0,0 +1,12 @@
+import 'package:dadafinanza/app_state.dart';
+import 'package:dadafinanza/data/app_database.dart';
+import 'package:dadafinanza/main.dart';
+import 'package:dadafinanza/screens/personal_settings_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('settings scaffold', (tester) async {
+    expect(true, isTrue);
+  });
+}
