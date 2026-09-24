f6ba9792e3907acfcf7a6d3c15a21d62354a6f55
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-25 00:39:43 +0200

    test: update widget service fake for currency sync

--- test/settings_csv_contract_test.dart
+++ test/settings_csv_contract_test.dart
@@ -15,6 +15,7 @@ class _NoopWidgetService extends WidgetService {
   @override
   Future<void> sync({
     required double balance,
+    required String currency,
     required List<Category> expenseCategories,
   }) async {}
 }
