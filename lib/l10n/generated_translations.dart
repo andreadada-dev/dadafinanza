29070f964aa7324b396bf545617729e550aaec51
Dada <63845798+andreadada@users.noreply.github.com>
2026-09-26 14:52:45 +0200

    Add generated translation map placeholder

---
+++ lib/l10n/generated_translations.dart
@@ -0,0 +1,5 @@
+const Map<String, Map<String, String>> generatedTranslations =
+    <String, Map<String, String>>{};
+
+const Map<String, Map<String, String>> generatedPhraseTranslations =
+    <String, Map<String, String>>{};
